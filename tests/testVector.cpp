6142b5d28bb07e2c6da268fc06e49cc4dc182eca
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-03-02 18:53:24 -0800

    fixing include ordering

--- tests/testVector.cpp
+++ tests/testVector.cpp
@@ -59,6 +59,7 @@
 // This header contains HiOp's MPI definitions
 #include <hiopVector.hpp>
 
+#include <hiopVector.hpp>
 #include "LinAlg/vectorTestsPar.hpp"
 // #include "LinAlg/vectorTestsRAJA.hpp"
 
