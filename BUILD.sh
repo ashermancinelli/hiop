9fa2cef7f7e6237309d25af833dfe7cf04804614
Slaven Peles <slaven.peles@pnnl.gov>
2020-04-15 21:43:27 -0700

    Update BUILD.sh file.

--- BUILD.sh
+++ BUILD.sh
@@ -9,9 +9,10 @@ export MY_CLUSTER=`uname -n | sed -e 's/[0-9]//g' -e 's/\..*//'`
 
 if [ "$MY_CLUSTER" == "newell" ]; then
     export MY_GCC_VERSION=7.4.0
-    export MY_CUDA_VERSION=9.2
+    export MY_CUDA_VERSION=10.2
     export MY_OPENMPI_VERSION=3.1.5
     export MY_CMAKE_VERSION=3.16.4
+    export MY_MAGMA_VERSION=2.5.2_cuda10.2
 else
     #  NOTE: The following is required when running from Gitlab CI via slurm job
     export MY_CLUSTER="marianas"
@@ -27,6 +28,9 @@ module load cuda/$MY_CUDA_VERSION
 module load openmpi/$MY_OPENMPI_VERSION
 module load cmake/$MY_CMAKE_VERSION
 
+export MY_RAJA_DIR=/qfs/projects/exasgd/$MY_CLUSTER/raja
+export MY_UMPIRE_DIR=/qfs/projects/exasgd/$MY_CLUSTER/umpire
+
 base_path=`dirname $0`
 #  NOTE: The following is required when running from Gitlab CI via slurm job
 if [ -z "$SLURM_SUBMIT_DIR" ]; then
@@ -35,13 +39,13 @@ fi
 
 #export MAKEFLAGS="-j 8"
 #export CMAKE_OPTIONS="-GNinja -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON"
-export CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DHIOP_USE_MPI=ON -DHIOP_DEEPCHECKS=ON"
+export CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DHIOP_USE_MPI=ON -DHIOP_DEEPCHECKS=ON -DHIOP_USE_RAJA=On -DRAJA_DIR=$MY_RAJA_DIR -DHIOP_ENABLE_UMPIRE=On -Dumpire_DIR=$MY_UMPIRE_DIR"
 
 BUILDDIR="build"
-rm -rf $BUILDDIR                || exit 1
-mkdir -p $BUILDDIR              || exit 1
-cd $BUILDDIR                    || exit 1
-cmake $CMAKE_OPTIONS ..         || exit 1
-cmake --build .                 || exit 1
-ctest                           || cat Testing/Temporary/LastTest.log
+rm -rf $BUILDDIR                            || exit 1
+mkdir -p $BUILDDIR                          || exit 1
+cd $BUILDDIR                                || exit 1
+CC=mpicc CXX=mpicxx cmake $CMAKE_OPTIONS .. || exit 1
+cmake --build .                             || exit 1
+ctest                                       || cat Testing/Temporary/LastTest.log
 exit 0
