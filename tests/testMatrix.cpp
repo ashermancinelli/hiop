b9dacd0ce696a7f1ffb7aed907d878d8af7f90be
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-05-29 17:14:14 -0700

    Resolved small issues with matrix unit tests addressed in #21 Also added tests specific to hiopMatrixDense

--- tests/testMatrix.cpp
+++ tests/testMatrix.cpp
@@ -1,3 +1,50 @@
+// Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory (LLNL).
+// Written by Cosmin G. Petra, petra1@llnl.gov.
+// LLNL-CODE-742473. All rights reserved.
+//
+// This file is part of HiOp. For details, see https://github.com/LLNL/hiop. HiOp 
+// is released under the BSD 3-clause license (https://opensource.org/licenses/BSD-3-Clause). 
+// Please also read “Additional BSD Notice” below.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// i. Redistributions of source code must retain the above copyright notice, this list 
+// of conditions and the disclaimer below.
+// ii. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the disclaimer (as noted below) in the documentation and/or 
+// other materials provided with the distribution.
+// iii. Neither the name of the LLNS/LLNL nor the names of its contributors may be used to 
+// endorse or promote products derived from this software without specific prior written 
+// permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Additional BSD Notice
+// 1. This notice is required to be provided under our contract with the U.S. Department 
+// of Energy (DOE). This work was produced at Lawrence Livermore National Laboratory under 
+// Contract No. DE-AC52-07NA27344 with the DOE.
+// 2. Neither the United States Government nor Lawrence Livermore National Security, LLC 
+// nor any of their employees, makes any warranty, express or implied, or assumes any 
+// liability or responsibility for the accuracy, completeness, or usefulness of any 
+// information, apparatus, product, or process disclosed, or represents that its use would
+// not infringe privately-owned rights.
+// 3. Also, reference herein to any specific commercial products, process, or services by 
+// trade name, trademark, manufacturer or otherwise does not necessarily constitute or 
+// imply its endorsement, recommendation, or favoring by the United States Government or 
+// Lawrence Livermore National Security, LLC. The views and opinions of authors expressed 
+// herein do not necessarily state or reflect those of the United States Government or 
+// Lawrence Livermore National Security, LLC, and shall not be used for advertising or 
+// product endorsement purposes.
 #include <iostream>
 #include <cassert>
 
@@ -20,11 +67,6 @@ int main(int argc, char** argv)
         printf("Support for MPI is enabled\n");
 #endif
 
-    /*
-    global_ordinal_type M_local = 10;
-    global_ordinal_type K_local = 15;
-    global_ordinal_type N_local = 100;
-    */
     global_ordinal_type M_local = 5 * numRanks;
     global_ordinal_type K_local = 2 * M_local;
     global_ordinal_type N_local = 10 * M_local;
@@ -104,39 +146,60 @@ int main(int argc, char** argv)
             fail += test.matrixTimesMat(A_mxk_local, A_kxn_local, A_mxn_local, rank);
             fail += test.matrixAddDiagonal(A_nxn, x_n_nodist, rank);
             fail += test.matrixAddSubDiagonal(A_nxn, x_m_nodist, rank);
+            fail += test.matrixAddToSymDenseMatrixUpperTriangle(A_nxn, A_mlxk, rank);
+            fail += test.matrixTransAddToSymDenseMatrixUpperTriangle(A_nxn, A_klxm, rank);
+            fail += test.matrixAddUpperTriangleToSymDenseMatrixUpperTriangle(A_nxn, A_mxm, rank);
 #ifdef HIOP_DEEPCHECKS
             fail += test.matrixAssertSymmetry(A_nxn, rank);
 #endif
+
+            // specific to matrixTestsDense
+            fail += test.matrixCopyBlockFromMatrix(A_mxm, A_kxn, rank);
+            fail += test.matrixCopyFromMatrixBlock(A_kxn, A_mxm, rank);
         }
 
         fail += test.matrixTransTimesMat(A_mxk_local, A_kxn, A_mxn, rank);
         fail += test.matrixTimesMatTrans(A_kxm, A_kxn_local, A_nxm, rank);
         fail += test.matrixAddMatrix(A_mxn, B_mxn, rank);
-        fail += test.matrixAddToSymDenseMatrixUpperTriangle(A_nxn, A_mlxk, rank);
-        fail += test.matrixTransAddToSymDenseMatrixUpperTriangle(A_nxn, A_klxm, rank);
-        fail += test.matrixAddUpperTriangleToSymDenseMatrixUpperTriangle(A_nxn, A_mxm, rank);
         fail += test.matrixMaxAbsValue(A_mxn, rank);
         fail += test.matrixIsFinite(A_mxn, rank);
         fail += test.matrixNumRows(A_mxn, M_global, rank);
         fail += test.matrixNumCols(A_mxn, N_global, rank);
+
+        // specific to matrixTestsDense
+        fail += test.matrixCopyFrom(A_mxn, B_mxn, rank);
+
+        // This matrix is allocated only for this test, since it requires
+        // more rows to be allocated with the constructor. Here we allocate
+        // one extra row for this purpose.
+        hiop::hiopMatrixDense A_mxn_extra_row(M_global, N_global, n_partition, comm, M_global+1);
+        fail += test.matrixAppendRow(A_mxn_extra_row, x_n_dist, rank);
+        fail += test.matrixCopyRowsFrom(A_kxn, A_mxn, rank);
+        fail += test.matrixShiftRows(A_mxn, rank);
+        fail += test.matrixReplaceRow(A_mxn, x_n_dist, rank);
+        fail += test.matrixGetRow(A_mxn, x_n_dist, rank);
     }
 
     // Test RAJA matrix
     {
-        // Code here ...
+      // Code here ...
     }
 
     if (rank == 0)
     {
-        if(fail)
-        {
-            std::cout << "Matrix tests failed\n";
-        }
-        else
-        {
-            std::cout << "Matrix tests passed\n";
-        }
+      if(fail)
+      {
+        std::cout << "Matrix tests failed\n";
+      }
+      else
+      {
+        std::cout << "Matrix tests passed\n";
+      }
     }
+
+    delete[] m_partition;
+    delete[] n_partition;
+    delete[] k_partition;
 
 #ifdef HIOP_USE_MPI
     MPI_Finalize();
--- tests/testVector.cpp
+++ tests/testVector.cpp
@@ -1,3 +1,50 @@
+// Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory (LLNL).
+// Written by Cosmin G. Petra, petra1@llnl.gov.
+// LLNL-CODE-742473. All rights reserved.
+//
+// This file is part of HiOp. For details, see https://github.com/LLNL/hiop. HiOp 
+// is released under the BSD 3-clause license (https://opensource.org/licenses/BSD-3-Clause). 
+// Please also read “Additional BSD Notice” below.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// i. Redistributions of source code must retain the above copyright notice, this list 
+// of conditions and the disclaimer below.
+// ii. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the disclaimer (as noted below) in the documentation and/or 
+// other materials provided with the distribution.
+// iii. Neither the name of the LLNS/LLNL nor the names of its contributors may be used to 
+// endorse or promote products derived from this software without specific prior written 
+// permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Additional BSD Notice
+// 1. This notice is required to be provided under our contract with the U.S. Department 
+// of Energy (DOE). This work was produced at Lawrence Livermore National Laboratory under 
+// Contract No. DE-AC52-07NA27344 with the DOE.
+// 2. Neither the United States Government nor Lawrence Livermore National Security, LLC 
+// nor any of their employees, makes any warranty, express or implied, or assumes any 
+// liability or responsibility for the accuracy, completeness, or usefulness of any 
+// information, apparatus, product, or process disclosed, or represents that its use would
+// not infringe privately-owned rights.
+// 3. Also, reference herein to any specific commercial products, process, or services by 
+// trade name, trademark, manufacturer or otherwise does not necessarily constitute or 
+// imply its endorsement, recommendation, or favoring by the United States Government or 
+// Lawrence Livermore National Security, LLC. The views and opinions of authors expressed 
+// herein do not necessarily state or reflect those of the United States Government or 
+// Lawrence Livermore National Security, LLC, and shall not be used for advertising or 
+// product endorsement purposes.
 #include <iostream>
 #include <assert.h>
 
@@ -35,20 +82,32 @@ int main(int argc, char** argv)
 
     global_ordinal_type Nlocal = 1000;
     global_ordinal_type Nglobal = Nlocal*numRanks;
-    partition = new global_ordinal_type [numRanks + 1];
-    partition[0] = 0;
+
+    auto n_partition = new global_ordinal_type [numRanks + 1];
+    n_partition[0] = 0;
     for(int i = 1; i < numRanks + 1; ++i)
-        partition[i] = i*Nlocal;
+        n_partition[i] = i*Nlocal;
+
+    global_ordinal_type Mlocal = 500;
+    global_ordinal_type Mglobal = Mlocal*numRanks;
+
+    auto m_partition = new global_ordinal_type [numRanks + 1];
+    m_partition[0] = 0;
+    for(int i = 1; i < numRanks + 1; ++i)
+        m_partition[i] = i*Mlocal;
 
     int fail = 0;
 
     // Test parallel vector
     {
-        hiop::hiopVectorPar x(Nglobal, partition, comm);
+        hiop::hiopVectorPar x(Nglobal, n_partition, comm);
         hiop::hiopVectorPar* y = x.alloc_clone();
         hiop::hiopVectorPar* z = x.alloc_clone();
         hiop::hiopVectorPar* a = x.alloc_clone();
         hiop::hiopVectorPar* b = x.alloc_clone();
+
+        // Allocate a vector smaller than x for testing copying operations
+        hiop::hiopVectorPar x_smaller(Mglobal, m_partition, comm);
         hiop::tests::VectorTestsPar test;
 
         fail += test.vectorGetSize(x, Nglobal, rank);
@@ -62,8 +121,8 @@ int main(int argc, char** argv)
         {
             fail += test.vectorCopyFromStarting(x, *y, rank);
             fail += test.vectorStartingAtCopyFromStartingAt(x, *y, rank);
-            fail += test.vectorCopyToStarting(x, *y, rank);
-            fail += test.vectorStartingAtCopyToStartingAt(x, *y, rank);
+            fail += test.vectorCopyToStarting(x, x_smaller, rank);
+            fail += test.vectorStartingAtCopyToStartingAt(x, x_smaller, rank);
         }
 
         fail += test.vectorTwonorm(x, rank);
@@ -120,6 +179,9 @@ int main(int argc, char** argv)
         else
             std::cout << "All tests passed\n";
     }
+
+    delete[] m_partition;
+    delete[] n_partition;
 
 #ifdef HIOP_USE_MPI
     MPI_Finalize();
